28cb4885f8a1a166fc80c56ea0f907c3720e0d7a
stal <stal@zodiaclabs.org>
2014-03-31 21:29:59 -0700

    Edit mio build instructions (thanks sn0w75)

--- objective_tox/testing/mio/buildit.sh
+++ objective_tox/testing/mio/buildit.sh
@@ -1,7 +1,6 @@
 cc -I../../core/toxcore -I../../core_extensions -I../../../txd_plus \
    -I../../../txd_plus/scrypt-jane -I/usr/local/include \
-   -DSCRYPT_CHACHA -DSCRYPT_KECCAK512 \
-   -no-integrated-as -march=native -L/usr/local/lib -lsodium \
+   -DSCRYPT_CHACHA -DSCRYPT_KECCAK512 -march=native --std=gnu99 \
    ../../core/toxcore/*.c ../../core_extensions/*.c ../../../txd_plus/*.c \
    ../../../txd_plus/scrypt-jane/scrypt-jane.c mio.c \
-   -o mio
+   -o mio -L/usr/local/lib -lsodium
